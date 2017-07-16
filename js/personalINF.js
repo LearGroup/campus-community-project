311e77b364760a2d45faacaa0e91c5c3d28d455b
MorningStar <792634614@qq.com>
2017-07-16 22:05:31 +0800

    update bottom nav

--- js/personalINF.js
+++ js/personalINF.js
@@ -1,9 +1,13 @@
+//mui.init([
+//	preloadPages:[{
+//  	url:'index.html',
+//  	id:'index.html',
+//  	style:{}
+//  }],
+//  preliadLimit:5
+//])
 mui.ready(function(){
 	document.querySelector('#indexButton').addEventListener('tap',function(){
-		mui.openWindow({
-			url:'index.html',
-			id:'index.html'
-		})
 		mui.back()
 	})
 })
