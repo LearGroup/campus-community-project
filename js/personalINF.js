734f6b1d4f793e81b12af12c73904944c19a04b4
MorningStar <792634614@qq.com>
2017-07-17 13:35:30 +0800

    fot test

--- js/personalINF.js
+++ js/personalINF.js
@@ -1,13 +1,32 @@
-//mui.init([
-//	preloadPages:[{
-//  	url:'index.html',
-//  	id:'index.html',
-//  	style:{}
-//  }],
-//  preliadLimit:5
-//])
-mui.ready(function(){
-	document.querySelector('#indexButton').addEventListener('tap',function(){
+mui.init({
+	preloadPages: [{
+			url: 'index.html',
+			id: 'index.html',
+					subpages: [{
+			url: 'child.html',
+			id: 'child.html',
+			styles: {
+				top: '55px',
+				bottom: '60px'
+			}
+		}]
+	}],
+	preliadLimit: 5
+})
+mui.ready(function() {
+	document.querySelector('#indexButton').addEventListener('tap', function() {
+		mui.openWindow({
+			url: 'index.html',
+			id: 'index.html',
+					subpages: [{
+			url: 'child.html',
+			id: 'child.html',
+			styles: {
+				top: '55px',
+				bottom: '60px'
+			}
+		}]
+		})
 		mui.back()
 	})
 })
