1ab9b92862d64bd116ee5b6c258d8001cdb09059
MorningStar <792634614@qq.com>
2017-07-22 10:27:46 +0800

    加入了登陆及注册，数据本地存放

--- js/child.js
+++ js/child.js
@@ -1,8 +1,5 @@
 mui.plusReady(function(){
-	var grelly = mui('.mui-slider');
-	grelly.slider({
-		interval: '2000'
-	})
+
 //	var imgHeight = document.querySelector("img.hotListItem").style.width;
 //	document.querySelector("img.hotListItem").style.height=imgHeight;
 	$('.hotListItem img').height($('.hotListItem img').width());
