6a4251e8b8e8afcf576a347230a83098d3b5a5ff
ChenWorkSpace <chen1494518303@outlook.com>
2017-07-24 23:32:21 +0800

    clean

---
+++ appFront/js/post.js
@@ -0,0 +1,7 @@
+	$.post("/Uncom/login_Login.action", {
+				use: sor,
+				user: $("#login_input_username").val(),
+				password: $("#login_input_password").val()
+			}, function(date) {
+			
+			})
