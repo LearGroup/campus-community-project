6d88db0dca1c9826f535f3e7c4caf98375857de2
ChenWorkSpace <chen1494518303@outlook.com>
2017-07-31 12:35:35 +0800

    m基于node.js 的即时通讯系统

--- appFront/js/post.js
+++
@@ -1,7 +0,0 @@
-	$.post("/Uncom/login_Login.action", {
-				use: sor,
-				user: $("#login_input_username").val(),
-				password: $("#login_input_password").val()
-			}, function(date) {
-			
-			})
