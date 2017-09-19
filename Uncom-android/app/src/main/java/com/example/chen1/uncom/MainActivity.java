107b8d2de3603516b826e83be48303c299c5b290
ChenWorkSpace <chen1494518303@outlook.com>
2017-09-19 10:37:29 +0800

    优化了整体风格

--- Uncom-android/app/src/main/java/com/example/chen1/uncom/MainActivity.java
+++ Uncom-android/app/src/main/java/com/example/chen1/uncom/MainActivity.java
@@ -29,6 +29,8 @@ import android.view.WindowManager;
 import android.widget.Toast;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
@@ -81,9 +83,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
-
         setContentView(R.layout.activity_main);
+        this.getWindow()
+                .getDecorView()
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
+        MIUISetStatusBarLightMode(this.getWindow(), true);
+        FlymeSetStatusBarLightMode(this.getWindow(), true);
         SharedPreferences sharedPreferences= this.getApplicationContext().getSharedPreferences("EMOTION_PREFS",MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
         if(sharedPreferences.getInt("startCount",-1)==-1){
@@ -140,6 +146,58 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         viewPager.setAdapter(sectionsAdapter);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
     }
+
+    public static boolean FlymeSetStatusBarLightMode(Window window, boolean dark) {
+        boolean result = false;
+        if (window != null) {
+            try {
+                WindowManager.LayoutParams lp = window.getAttributes();
+                Field darkFlag = WindowManager.LayoutParams.class
+                        .getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
+                Field meizuFlags = WindowManager.LayoutParams.class
+                        .getDeclaredField("meizuFlags");
+                darkFlag.setAccessible(true);
+                meizuFlags.setAccessible(true);
+                int bit = darkFlag.getInt(null);
+                int value = meizuFlags.getInt(lp);
+                if (dark) {
+                    value |= bit;
+                } else {
+                    value &= ~bit;
+                }
+                meizuFlags.setInt(lp, value);
+                window.setAttributes(lp);
+                result = true;
+            } catch (Exception e) {
+
+            }
+        }
+        return result;
+    }
+
+    public static boolean MIUISetStatusBarLightMode(Window window, boolean dark) {
+        boolean result = false;
+        if (window != null) {
+            Class clazz = window.getClass();
+            try {
+                int darkModeFlag = 0;
+                Class layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+                Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+                darkModeFlag = field.getInt(layoutParams);
+                Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+                if (dark) {
+                    extraFlagField.invoke(window, darkModeFlag, darkModeFlag);//状态栏透明且黑色字体
+                } else {
+                    extraFlagField.invoke(window, 0, darkModeFlag);//清除黑色字体
+                }
+                result = true;
+            } catch (Exception e) {
+
+            }
+        }
+        return result;
+    }
+
 
 
     public class SectionsAdapter extends FragmentPagerAdapter{
